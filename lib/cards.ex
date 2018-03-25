1d386776620ada4067448231c4e4d3a9a29f402c
Ben Ricker <bgricker@gmail.com>
2018-03-25 10:56:48 -0400

    add values/suits, output array of cards in create_deck

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,7 +1,12 @@
 defmodule Cards do
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+  for suit <- suits, value <- values do
+    "#{value} of #{suit}"
   end
+end
 
   def shuffle(deck) do
     Enum.shuffle(deck)
